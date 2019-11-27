8e0945ad44b51e3534bf593f58fb52491de94862
Rich Lott / Artful Robot <forums@artfulrobot.uk>
2019-11-27 12:34:07 +0000

    od: finally some semblence of order

--- js/od-mailing-unsubscribe.js
+++ js/od-mailing-unsubscribe.js
@@ -1,13 +1,9 @@
-;;
-
-(function ($) {
-  return $(function () {
-    $('.messages.status:contains("Are you sure you want to be removed")').remove();
-    var $success = $('.messages.status:contains("has been successfully unsubscribed")');
-    if ($success.length) {
-      $('body').addClass('od-unsubscribe-successful');
-      $('#page-title').text('Unsubscribed.');
-    }
-  });
-})(jQuery);
+($ => $(() => {
+  $('.messages.status:contains("Are you sure you want to be removed")').remove();
+  const $success = $('.messages.status:contains("has been successfully unsubscribed")');
+  if ($success.length) {
+    $('body').addClass('od-unsubscribe-successful');
+    $('#page-title').text('Unsubscribed.');
+  }
+}))(jQuery);
 //# sourceMappingURL=od-mailing-unsubscribe.js.map
