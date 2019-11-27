912427fb66323acb8f945c7fcc62606aa30280ed
Rich Lott / Artful Robot <forums@artfulrobot.uk>
2019-11-27 10:51:44 +0000

    fix template name

--- template.php
+++ template.php
@@ -17,19 +17,19 @@ function aaah_preprocess_page(&$variables) {
   $aaah_path = drupal_get_path('theme', 'aaah');
 
   if (theme_get_setting('shoreditch_enabled')) {
-    $variables['page']['content']['#attached']['css']['civicrm'] = [
+    $variables['page']['content']['#attached']['css']['aaah_civicrm'] = [
       'data' => $aaah_path . "/css/aaah-civicrm-shoreditch.css",
       'weight' => 1000,
     ];
   }
   else {
-    $variables['page']['content']['#attached']['css']['civicrm'] = [
+    $variables['page']['content']['#attached']['css']['aaah_civicrm'] = [
       'data' => $aaah_path . "/css/aaah-civicrm-no-shoreditch.css",
       'weight' => 1000,
     ];
   }
 
-  $variables['page']['content']['#attached']['css']['civicrm'] = [
+  $variables['page']['content']['#attached']['css']['aaah_local'] = [
     'data' => $aaah_path . "/css/aaah-local.css",
     'weight' => 1001,
   ];
