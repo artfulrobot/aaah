410cf6ee8cc193ee92a24b6a093051740e674e38
Rich Lott / Artful Robot <forums@artfulrobot.uk>
2019-12-17 16:18:40 +0000

    stripped out CiviCRM stuff

--- template.php
+++ template.php
@@ -1,10 +1,5 @@
 <?php
 
-function aaah_preprocess_html(&$variables) {
-  if (theme_get_setting('shoreditch_enabled')) {
-    $variables['classes'] = 'civicrm-shoreditch';
-  }
-}
 function aaah_preprocess_page(&$variables) {
   if (!isset($variables['page']['content']['#attached']['css']['fonts'])) {
     // Allow subthemes to set this and for us to not override it (as this
@@ -15,19 +10,6 @@ function aaah_preprocess_page(&$variables) {
     ];
   }
   $aaah_path = drupal_get_path('theme', 'aaah');
-
-  if (theme_get_setting('shoreditch_enabled')) {
-    $variables['page']['content']['#attached']['css']['aaah_civicrm'] = [
-      'data' => $aaah_path . "/css/aaah-civicrm-shoreditch.css",
-      'weight' => 1000,
-    ];
-  }
-  else {
-    $variables['page']['content']['#attached']['css']['aaah_civicrm'] = [
-      'data' => $aaah_path . "/css/aaah-civicrm-no-shoreditch.css",
-      'weight' => 1000,
-    ];
-  }
 
   $variables['page']['content']['#attached']['css']['aaah_local'] = [
     'data' => $aaah_path . "/css/aaah-local.css",
