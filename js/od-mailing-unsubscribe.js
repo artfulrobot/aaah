1b61b3c1c98af5b876f5ba15127543fa46dea037
Rich Lott / Artful Robot <forums@artfulrobot.uk>
2020-04-15 07:44:17 +0100

    od: recompiled assets

--- js/od-mailing-unsubscribe.js
+++ js/od-mailing-unsubscribe.js
@@ -1,9 +1,13 @@
-($ => $(() => {
-  $('.messages.status:contains("Are you sure you want to be removed")').remove();
-  const $success = $('.messages.status:contains("has been successfully unsubscribed")');
-  if ($success.length) {
-    $('body').addClass('od-unsubscribe-successful');
-    $('#page-title').text('Unsubscribed.');
-  }
-}))(jQuery);
+;;
+
+(function ($) {
+  return $(function () {
+    $('.messages.status:contains("Are you sure you want to be removed")').remove();
+    var $success = $('.messages.status:contains("has been successfully unsubscribed")');
+    if ($success.length) {
+      $('body').addClass('od-unsubscribe-successful');
+      $('#page-title').text('Unsubscribed.');
+    }
+  });
+})(jQuery);
 //# sourceMappingURL=od-mailing-unsubscribe.js.map
