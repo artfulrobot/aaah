61acc06b3ada317bf04d68ded329618ef3a863d2
Rich Lott / Artful Robot <forums@artfulrobot.uk>
2021-01-21 14:51:41 +0000

    od: add Google Tag Manager code GTM

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -21,8 +21,10 @@
 
   <?php /* oD {{{ */ ?>
   <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
+
   <!-- Global site tag (gtag.js) - Google Analytics -->
   <script async src="https://www.googletagmanager.com/gtag/js?id=UA-379949-2"></script>
+
   <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
@@ -30,10 +32,25 @@
 
     gtag('config', 'UA-379949-2', { 'optimize_id': 'GTM-KW33TL8'});
   </script>
+
+  <?php /* this next chunk added 21 Jan 2021 on Matt's request */ ?>
+  <!-- Google Tag Manager - Other purposes -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-5DLKDFS');</script>
+  <!-- End Google Tag Manager -->
   <?php /* oD }}} */ ?>
 
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
+  <?php /* This next chunk added 21 Jan 2021 as Matt requested.*/ ?>
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5DLKDFS"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
+
   <?php print $page_top; ?>
   <?php print $page; ?>
   <?php print $page_bottom; ?>
